a0b967da99446f8d9c99c3c80aa23527149d7031
Daniel Mutis <64042203+DanielMutis85@users.noreply.github.com>
2021-05-10 15:36:41 -0500

    Ajuste contenido

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -286,7 +286,7 @@
       .col-auto
         h5.text-center.mb-5 Factores de riesgo y afectaciones del trabajo infantil    
         figure
-          img(src="@/assets/tema_3/img_3_1_11.svg", alt="Factores de riesgo y afectaciones del trabajo infantil").mb-4
+          img(src="@/assets/tema_3/img_3_1_11.svg", alt="Factores de riesgo y afectaciones del trabajo infantil", style="width:750px;").mb-4
           figcaption Nota: adaptado de ACNUR (2019). 
 
     p.mb-5 A nivel nacional las cifras de trabajo infantil presentan un panorama inquietante al encontrarse que para el año 2017 un consolidado de 796.000 niños, niñas y adolescentes estaban en situación de trabajo infantil. La población de adolescentes de 15 a 17 años es aquella que se encuentran más afectada por esta problemática, al concentrar el 19,8 % del total (ICBF, 2018). Teniendo en cuenta todo lo anterior, se hace necesario emprender acciones para la superación de esta problemática. 
